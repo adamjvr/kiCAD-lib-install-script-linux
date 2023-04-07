901f729c96c4668f45456f6f22063f5dc6f4b191
adamjvr <fenderstratman@Gmail.com>
2023-04-06 21:12:58 -0400

    added ability to take github URL for kiCAD Library as commandline argument

--- src/kicad_lib_installer.py
+++ src/kicad_lib_installer.py
@@ -1,25 +1,32 @@
 import os
 import urllib.request
 import zipfile
+import argparse
+
+# parse the Github URL argument
+parser = argparse.ArgumentParser(description='Install KiCAD footprint and symbol libraries from a Github repository.')
+parser.add_argument('github_url', metavar='url', type=str, help='the Github URL for the KiCAD library')
+args = parser.parse_args()
 
 # define the library names and URLs
-footprint_library_name = 'my_footprint_library'
-footprint_library_url = 'https://example.com/my_footprint_library.zip'
-symbol_library_name = 'my_symbol_library'
-symbol_library_url = 'https://example.com/my_symbol_library.zip'
+library_name = args.github_url.split("/")[-1].split(".")[0]
+footprint_library_url = f'{args.github_url}/archive/master.zip'
+symbol_library_url = f'{args.github_url}-symbols/archive/master.zip'
 
 # define the path to the KiCAD footprint and symbol libraries folders
 footprint_libraries_folder_path = f'/home/adam/.local/share/kicad/modules'
 symbol_libraries_folder_path = f'/home/adam/.local/share/kicad/symbols'
 
 # download and extract the footprint library zip file
-urllib.request.urlretrieve(footprint_library_url, f"{footprint_library_name}.zip")
-with zipfile.ZipFile(f"{footprint_library_name}.zip", 'r') as zip_ref:
+urllib.request.urlretrieve(footprint_library_url, f"{library_name}.zip")
+with zipfile.ZipFile(f"{library_name}.zip", 'r') as zip_ref:
+    zip_ref.extractall(f"{library_name}")
     zip_ref.extractall(footprint_libraries_folder_path)
-os.remove(f"{footprint_library_name}.zip")
+os.remove(f"{library_name}.zip")
 
 # download and extract the symbol library zip file
-urllib.request.urlretrieve(symbol_library_url, f"{symbol_library_name}.zip")
-with zipfile.ZipFile(f"{symbol_library_name}.zip", 'r') as zip_ref:
+urllib.request.urlretrieve(symbol_library_url, f"{library_name}_symbols.zip")
+with zipfile.ZipFile(f"{library_name}_symbols.zip", 'r') as zip_ref:
+    zip_ref.extractall(f"{library_name}-symbols")
     zip_ref.extractall(symbol_libraries_folder_path)
-os.remove(f"{symbol_library_name}.zip")
+os.remove(f"{library_name}_symbols.zip")
